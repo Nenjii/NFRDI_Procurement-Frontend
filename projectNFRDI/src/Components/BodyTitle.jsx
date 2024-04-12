cf582ac1672cdfb0a1a0421b267912c74a480a75
Nino Duque <oninpoh@gmail.com>
2024-04-12 16:18:11 +0800

    12.04.2024 Add some designs

--- projectNFRDI/src/Components/BodyTitle.jsx
+++ projectNFRDI/src/Components/BodyTitle.jsx
@@ -1,10 +1,21 @@
 import style from "../Components/BodyTitle.module.css";
+import { ReactTyped } from "react-typed";
 
 const BodyTitle = () => {
   return (
     <div className={style.bodyTitle}>
       <div className={style.bodyContentTitle1}>Welcome to the</div>
-      <div className={style.bodyContentTitle2}>Procurement Monitoring</div>
+      <ReactTyped
+        className={style.bodyContentTitle2}
+        strings={[
+          "Procurement Monitoring",
+          "DA-NFRDI",
+          "National Fisheries Research and Development Institute",
+        ]}
+        typeSpeed={40}
+        backSpeed={40}
+        loop
+      ></ReactTyped>
       <div className={style.bodyContentTitle3}>Reports</div>
     </div>
   );
